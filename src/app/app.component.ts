fdca57a9700bc9f2d6a8b117e502d4f02137d103
Oleh Soklakov <oleh_soklakov@epam.com>
2018-05-31 15:56:50 +0300

    create the product service and inject it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {ProductService} from "./products/product.service";
 
 @Component({
   selector: 'pm-root', /*name of component in HTML*/
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'Acme Product Management';
