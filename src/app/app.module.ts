139ec21e1a671004a50a1f71a3560fb21ae9360b
Oleh Soklakov <oleh_soklakov@epam.com>
2018-05-31 15:21:49 +0300

    Pass input param from component to another component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppComponent } from './app.component';
 import {ProductListComponent} from "./products/product-list.component";
 import {FormsModule} from "@angular/forms";
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {StarComponen} from "./shared/star.componen";
+import {StarComponent} from "./shared/star.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponen
+    StarComponent
   ],
   imports: [
     BrowserModule,
