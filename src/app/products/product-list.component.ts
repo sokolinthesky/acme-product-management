c19ea7a27365f84c89375d3f188eb84be9c30b8a
Oleh Soklakov <oleh_soklakov@epam.com>
2018-06-02 15:33:42 +0300

    add basic Routing and Navigation

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import {Product} from "./product";
 import {ProductService} from "./product.service";
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
