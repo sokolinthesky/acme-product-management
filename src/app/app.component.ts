bfe17c067347a0ac1a90490d6c8c15ef06af5d52
Oleh Soklakov <oleh_soklakov@epam.com>
2018-06-04 15:23:53 +0300

    Create Product Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {ProductService} from "./products/product.service";
 
 @Component({
   selector: 'pm-root', /*name of component in HTML*/
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ProductService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Acme Product Management';
---
+++ src/app/products/product.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ProductListComponent} from "./product-list.component";
+import {ProductDetailComponent} from "./product-detail.component";
+import {ConvertToSpacesPipe} from "../shared/convert-to-spaces.pipe";
+import {StarComponent} from "../shared/star.component";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {ProductGuardService} from "./product-guard.service";
+import {ProductService} from "./product.service";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id',
+        canActivate: [ ProductGuardService ],
+        component: ProductDetailComponent },
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
+  providers: [
+    ProductService,
+    ProductGuardService
+  ]
+})
+export class ProductModule { }
